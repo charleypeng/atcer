3b3b54d849c1ce3d8351ae12f1045b528da6d402
Charley Peng <charleypeng@gmail.com>
2022-12-26 11:28:00 +0800

    format:minor

--- src/Framework/ATCer.Core/Base/Domains/Function.cs
+++ src/Framework/ATCer.Core/Base/Domains/Function.cs
@@ -4,8 +4,6 @@
 //  CopyRight(C) 2022  版权所有 
 // -----------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using MyHttpMethod = ATCer.Enums.MyHttpMethod;
--- src/Framework/ATCer.Core/Base/Utils/IdentityUtil.cs
+++ src/Framework/ATCer.Core/Base/Utils/IdentityUtil.cs
@@ -30,7 +30,7 @@ namespace ATCer.Base
         /// 
         /// </summary>
         /// <returns></returns>
-        public static string GetIdentityId()
+        public static string? GetIdentityId()
         {
             return GetIdentity()?.Id;
         }
