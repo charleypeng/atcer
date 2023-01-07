cb3c1dee8a37854149bb7391e0cb9dc880937838
Charley Peng <charleypeng@gmail.com>
2023-01-07 20:55:59 +0800

    add:bulkextension(not finished)

---
+++ src/Framework/ATCer.Core/Base/Extentions/BulkExtension.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------------
+// ATCer 全平台综合性空中交通管理系统
+//  作者：彭磊  
+//  CopyRight(C) 2022  版权所有 
+// -----------------------------------------------------------------------------
+
+using Furion.DatabaseAccessor;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATCer.Core.Base.Extentions
+{
+    public static class BulkExtension
+    {
+        public static async Task MyBulkInsertOrUpdate<T>(this IQueryable<T> query, CancellationToken cancellationToken = default)
+        {
+            await query.ExecuteDeleteAsync(cancellationToken);
+        }
+    }
+}
